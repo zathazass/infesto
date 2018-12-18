5501ebd507b2d6abff23e6cbccc05bd3a7b9ac5b
zathazass <34094585+zathazass@users.noreply.github.com>
2018-12-18 14:34:47 +0530

    Update settings.py

--- infesto/settings.py
+++ infesto/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 #email authentication
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'infesto2k19@gmail.com'
-EMAIL_HOST_PASSWORD = 'infesto@it'
+EMAIL_HOST_USER = 'youremail'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
